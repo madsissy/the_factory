f1c8b07a234fe2251f9559bda9c4df6fb4dd9125
madsissy <romansubbi@gmail.com>
2019-08-22 23:15:42 +0200

    wip players crud

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,34 @@
+class PlayersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @players = current_user.players
+  end
+
+  def new
+    @player = current_user.players.new
+  end
+
+  def create
+    @player = current_user.players.new(players_params)
+
+    if @player.save
+      redirect_to player_intro_path(@player)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @player = Player.find params[:id]
+    @player.destroy
+
+    redirect_to players_path
+  end
+
+  private
+
+    def players_params
+      params.require(:player).permit()
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  resources :players, only: [:index, :new, :create, :destroy] do
+    resource :intro, only: [:show]
+  end
+
 end
