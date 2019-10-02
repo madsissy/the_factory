1f870b0cf45f908e8403055b1ba28be2ebe5ae47
madsissy <romansubbi@gmail.com>
2019-10-02 10:24:28 +0200

    sissies routes & controller

---
+++ app/controllers/players/sissies_controller.rb
@@ -0,0 +1,16 @@
+class Players::SissiesController < Players::BaseController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  resources :players, only: [:index, :new, :create, :destroy] do
-    resource :intro, only: [:show], to: 'players/intros#show'
-    resource :houses, only: [:show], to: 'players/houses#show'
+  resources :players,    only: [:index, :new, :create, :destroy] do
+    resource :intro,     only: [:show], to: 'players/intros#show'
+    resource :houses,    only: [:show], to: 'players/houses#show'
     resource :factories, only: [:show], to: 'players/factories#show'
+    resources :sissies,  controller: 'players/sissies'
   end
 
 end
