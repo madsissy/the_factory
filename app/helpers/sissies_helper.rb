6df331b01e5de5498c7ff79e2d3017ad8b538452
madsissy <romansubbi@gmail.com>
2019-10-05 14:26:04 +0200

    sissies helper

---
+++ app/helpers/sissies_helper.rb
@@ -0,0 +1,49 @@
+module SissiesHelper
+
+  def height_in_words sissy
+    case
+    when sissy.height.between?(150, 165) then 'small'
+    when sissy.height.between?(166, 180) then 'average'
+    when sissy.height.between?(181, 190) then 'tall'
+    end
+  end
+
+  def weight_in_words sissy
+    case
+    when sissy.weight.between?(50, 75)  then 'thin'
+    when sissy.weight.between?(76, 95)  then 'average'
+    when sissy.weight.between?(96, 120) then 'fat'
+    end
+  end
+
+  def hair_length_in_words sissy
+    case
+    when sissy.hair_length.zero?              then 'shaved'
+    when sissy.hair_length.between?(1, 10)    then 'very short'
+    when sissy.hair_length.between?(11, 30)   then 'short'
+    when sissy.hair_length.between?(31, 50)   then 'shoulder length'
+    when sissy.hair_length.between?(61, 90)   then 'long'
+    when sissy.hair_length.between?(91, 120)  then 'very long'
+    end
+  end
+
+  def penis_size_in_words sissy
+    case
+    when sissy.penis_size.between?(1, 4)     then 'micro'
+    when sissy.penis_size.between?(5, 9)     then 'tiny'
+    when sissy.penis_size.between?(10, 12)   then 'small'
+    when sissy.penis_size.between?(13, 18)   then 'average'
+    when sissy.penis_size.between?(19, 23)   then 'big'
+    when sissy.penis_size.between?(24, 30)   then 'huge'
+    end
+  end
+
+  def attractiveness_in_words sissy
+    case
+    when sissy.attractiveness.between?(0, 25)   then 'ugly'
+    when sissy.attractiveness.between?(26, 50)  then 'quite ugly'
+    when sissy.attractiveness.between?(51, 75)  then 'quite attractive'
+    when sissy.attractiveness.between?(76, 100) then 'attractive'
+    end
+  end
+end
---
+++ spec/helpers/sissies_helper_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe SissiesHelper do
+  let(:sissy) { build(:sissy) }
+
+  describe '#height_in_words' do
+    it 'returns a word for sissy\'s height' do
+      sissy.height = 150
+      expect(height_in_words(sissy)).to eq 'small'
+      sissy.height = 166
+      expect(height_in_words(sissy)).to eq 'average'
+      sissy.height = 181
+      expect(height_in_words(sissy)).to eq 'tall'
+    end
+  end
+
+  describe '#weight_in_words' do
+    it 'returns a word for sissy\'s weight' do
+      sissy.weight = 50
+      expect(weight_in_words(sissy)).to eq 'thin'
+      sissy.weight = 95
+      expect(weight_in_words(sissy)).to eq 'average'
+      sissy.weight = 96
+      expect(weight_in_words(sissy)).to eq 'fat'
+    end
+  end
+
+  describe '#hair_length_in_words' do
+    it 'returns a word for sissy\'s hair_length' do
+      sissy.hair_length = 0
+      expect(hair_length_in_words(sissy)).to eq 'shaved'
+      sissy.hair_length = 1
+      expect(hair_length_in_words(sissy)).to eq 'very short'
+      sissy.hair_length = 11
+      expect(hair_length_in_words(sissy)).to eq 'short'
+      sissy.hair_length = 31
+      expect(hair_length_in_words(sissy)).to eq 'shoulder length'
+      sissy.hair_length = 61
+      expect(hair_length_in_words(sissy)).to eq 'long'
+      sissy.hair_length = 91
+      expect(hair_length_in_words(sissy)).to eq 'very long'
+    end
+  end
+
+  describe '#penis_size_in_words' do
+    it 'returns a word for sissy\'s penis_size' do
+      sissy.penis_size = 1
+      expect(penis_size_in_words(sissy)).to eq 'micro'
+      sissy.penis_size = 5
+      expect(penis_size_in_words(sissy)).to eq 'tiny'
+      sissy.penis_size = 10
+      expect(penis_size_in_words(sissy)).to eq 'small'
+      sissy.penis_size = 13
+      expect(penis_size_in_words(sissy)).to eq 'average'
+      sissy.penis_size = 19
+      expect(penis_size_in_words(sissy)).to eq 'big'
+      sissy.penis_size = 24
+      expect(penis_size_in_words(sissy)).to eq 'huge'
+    end
+  end
+
+  describe '#attractiveness_in_words' do
+    it 'returns a word for sissy\'s attractiveness' do
+      sissy.attractiveness = 0
+      expect(attractiveness_in_words(sissy)).to eq 'ugly'
+      sissy.attractiveness = 26
+      expect(attractiveness_in_words(sissy)).to eq 'quite ugly'
+      sissy.attractiveness = 51
+      expect(attractiveness_in_words(sissy)).to eq 'quite attractive'
+      sissy.attractiveness = 76
+      expect(attractiveness_in_words(sissy)).to eq 'attractive'
+    end
+  end
+end
