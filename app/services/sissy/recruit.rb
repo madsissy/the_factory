5602295e00c7468f3171c8ca1d676064e9689542
madsissy <romansubbi@gmail.com>
2019-10-06 10:54:02 +0200

    recruit view

--- app/services/sissy/recruit.rb
+++ app/services/sissy/recruit.rb
@@ -2,6 +2,10 @@ module Sissy::Recruit
 
   def recruit
     # First simple rule before evolution to a complex system with items
-    (player.intimidation >= sissy.will) || (player.seduction >= sissy.will)
+    result = (player.intimidation >= sissy.will) || (player.seduction >= sissy.will)
+
+    sissy.update(owned: true) if result
+
+    result
   end
 end
--- spec/services/sissy/recruit_spec.rb
+++ spec/services/sissy/recruit_spec.rb
@@ -8,16 +8,18 @@ RSpec.describe Sissy::Recruit do
     context 'player seduction is greater or equal to sissy\'s will' do
       before{ sissy.player.update(seduction: 50) }
 
-      it 'returns true' do
+      it 'returns true and sets owned' do
         expect(subject.recruit).to eq true
+        expect(sissy.owned    ).to eq true
       end
     end
 
     context 'player intimidation is greater or equal to sissy\'s will' do
       before{ sissy.player.update(seduction: 60) }
 
-      it 'returns true' do
+      it 'returns true and sets owned' do
         expect(subject.recruit).to eq true
+        expect(sissy.owned    ).to eq true
       end
     end
 
